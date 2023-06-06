b8c5877fd8a6732f9ff5729c8d5695f329f64d44
Aldais <fabien.lauroua@aldais.com>
2023-06-06 17:31:44 +0000

    Correction sur les action automatique  DEV-02-AC-02-01 et  DEV-02-AC-02-02 / DEV :  DEV-03-MO-SO-03 et   DEV-03-MO-SO-04 sur la mise à jours de la date effective et shipped sur la base de la plus lointaine pour les transfert concernant le SO

--- sol_specific/models/purchase_order.py
+++ sol_specific/models/purchase_order.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import datetime
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -19,10 +20,26 @@ class StockPicking(models.Model):
     # " SOL_2023.01 - DEV-02-AC-01-01 : Purchase_order.py > StockPicking > change_po_schedule_date(self)
     #A partir d'un transfert ""copié"" sa schedule date dans le PO associé "
     def change_po_scheduled_date(self):
+        _logger.info('------------------------')
+        _logger.info('change_po_scheduled_date %s et %s', self.sale_id, self.scheduled_date)
+        _logger.info('------------------------')
         self.purchase_id.scheduled_date = self.scheduled_date
     
     #SOL_2023.01 - DEV-02-AC-01-02 : Purchase_order.py > StockPicking > change_po_date_done(self)
-    #A partir d'un transfert ""copier"" son effective date dans le PO associé "
+    #A partir d'un transfert ""copier"" son effective date dans le PO associé. "
+
     def change_po_date_done(self):
-        self.purchase_id.date_done = self.effective_shipment_date
+        aged_effective_date = datetime.date(2000, 1, 1)
+        _logger.info('------------------------')
+        _logger.info('change_po_date_done')
+        _logger.info('------------------------')
+        for transfert in self.purchase_id.picking_ids:
+            _logger.info('------------------------')
+            _logger.info('change_po_date_done %s et %s', transfert.effective_shipment_date, aged_effective_date)
+            _logger.info('------------------------')
+            if (transfert.effective_shipment_date is not False):
+                if (transfert.effective_shipment_date > aged_effective_date):
+                    aged_effective_date = transfert.effective_shipment_date
+        self.purchase_id.date_done = aged_effective_date
+        #self.purchase_id.date_done = self.effective_shipment_dat
         
