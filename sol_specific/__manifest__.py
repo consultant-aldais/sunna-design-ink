698aaa494e3f3e1a4097ed5899ea10d262c47976
Aldais <fabien.lauroua@aldais.com>
2024-02-14 09:39:47 +0000

    sol_specific: Change Version ID

--- sol_specific/__manifest__.py
+++ sol_specific/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'productivity',
-    'version': 'SOL.2023.2',
+    'version': '17.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','purchase'],
